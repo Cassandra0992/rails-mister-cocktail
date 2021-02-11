fb7629baa6e2f5e80abcb1e54ff3bdd30f8845b6
Cassandra Mansiamina <mansiamina.cassandra@hotmail.fr>
2021-02-11 12:56:56 +0100

    add cocktail controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_serialized = open(url).read
+list_drinks = JSON.parse(drinks_serialized)
+
+my_drinks = list_drinks['drinks']
+
+puts 'Creating drinks...'
+
+ingredients = []
+my_drinks.each do |drink|
+  ingredients << drink["strIngredient1"]
+end
+10.times do
+  ingredients.each do |ingredient|
+    Ingredient.create(name: ingredient)
+  end
+end
+
+puts "Finished!"
